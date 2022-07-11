4cbb3eab7d9f46c3327a34a49e13b5ecd016660e
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-07-11 11:07:52 -0400

    filtrado de fichas vacias

--- scripts/R/unir-datos.R
+++ scripts/R/unir-datos.R
@@ -16,8 +16,19 @@ lista_temas <- readRDS("temas/lista_temas.Rds")
 fichas_todo <- read_csv("fichas/fichas_todo.csv")
 
 lista_temas_fichas <- lista_temas %>%
-  map(
-    ~left_join(.x, fichas_todo, by = "cuce")
-  )
+  map(~left_join(.x, fichas_todo, by = "cuce")) %>%
+  map(~filter(.x, !is.na(descripcion_del_bien_o_servicio)))
 
-walk2(lista_temas_fichas, names(lista_temas_fichas), ~write_csv(.x, paste0("temas/", .y, ".csv")))
+saveRDS(lista_temas_fichas, "temas/lista_temas_fichas.Rds")
+
+walk2(lista_temas_fichas, names(lista_temas_fichas), ~write_csv(.x, paste0("temas/", .y, ".csv"), na = ""))
+
+# contar datos que existen
+lista_temas_fichas <- readRDS("temas/lista_temas_fichas.Rds")
+df_temas_fichas <- bind_rows(lista_temas_fichas, .id = "tema") %>%
+  arrange(fecha_publicacion)
+
+n_distinct(df_temas_fichas$cuce)
+
+df_temas_fichas %>%
+  count(tema, sort = T)
