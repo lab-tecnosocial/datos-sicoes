c0671223b95f28372f84711bdc7c3f1046598e99
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-07-24 22:07:01 -0400

    data: tablas para plataforma

--- scripts/R/unir-temas.R
+++ scripts/R/unir-temas.R
@@ -1,12 +1,62 @@
 library(tidyverse)
+library(jsonlite)
 
+# Cargar archivos y contar
 archivos <- list.files("temas/datos-drive/", full.names = T) %>%
   set_names(tools::file_path_sans_ext(basename(.)))
 
+pdfs <- list.files("scripts/python/fichas_simp/", recursive = T) %>% 
+  basename() %>%
+  unique()
+
+# Unir temas
 df_temas <- map_dfr(archivos, read_csv, .id = "tema") %>%
   arrange(fecha_publicacion) %>%
   relocate(tema)
 
-df_temas_cuce <- df_temas %>% distinct(cuce, .keep_all = T)
+# Funcion para exportar a CSV y JSON
 
-write_csv(df_temas, "temas/datos-drive/temas_cuce.csv")
+export_csv_json <- function(df, path = "todo/covid/") {
+  name_df <- deparse(substitute(df))
+  write_csv(df, paste0(path, name_df, ".csv" ), na = "")
+  write(toJSON(df, pretty = T), paste0(path, name_df, ".json"))
+}
+
+# Exportar cuces unicos (3632)
+conv_cuces <- df_temas %>% distinct(cuce, .keep_all = T) %>%
+  select(cuce:normativa) %>%
+  rename(estado = estado.x)
+# Exportar temas (4716)
+conv_temas <- df_temas %>%
+  distinct(tema, cuce, .keep_all = T) %>%
+  select(tema:normativa) %>%
+  rename(estado = estado.x)
+export_csv_json(conv_temas)
+
+# Temas en convocatorias
+cuces_temas <- conv_temas %>%
+  group_by(cuce) %>%
+  summarize(temas = paste(tema, collapse = ", "))
+export_csv_json(cuces_temas)
+
+# Añadir temas a cuces unicos
+conv_cuces <- conv_cuces %>%
+  left_join(cuces_temas) %>%
+  relocate(temas)
+export_csv_json(conv_cuces)
+
+# Exportar items
+fichas_todo <- read_csv("fichas/fichas_todo.csv")
+fichas <- fichas_todo %>%
+  filter(cuce %in% conv_cuces$cuce)
+export_csv_json(fichas)
+
+# Exportar adjudicados
+adjudicados_todo <- read_csv("scripts/python/out/v1/tablas_adjudicado.csv")
+adjudicados <- adjudicados_todo %>%
+  filter(cuce %in% conv_cuces$cuce) %>%
+  select(-list_n)
+export_csv_json(adjudicados)
+
+# Joins conv y fichas
+
