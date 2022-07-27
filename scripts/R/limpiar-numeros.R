bd48fd0f36d7682b4aa794d058fc345aae00f179
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-07-27 12:26:13 -0400

    limpieza adjudicados

--- scripts/R/limpiar-numeros.R
+++ scripts/R/limpiar-numeros.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(jsonlite)
 totales <- read_csv("todo/covid/tablas_totales.csv")
 
 parsear_numero_diverso <- function(n) {
@@ -14,3 +15,16 @@ totales_clean <- totales %>%
     monto_c = parsear_numero_diverso(monto_c)
   )
 
+export_csv_json(totales_clean)
+
+
+adjudicados <- read_csv("todo/covid/adjudicados.csv", col_types = "cccccccc")
+
+adjudicados <- adjudicados %>%
+  mutate(
+    monto_adjudicado = parsear_numero_diverso(monto_adjudicado),
+    monto_de = parsear_numero_diverso(monto_de),
+    monto = parsear_numero_diverso(monto)
+  )
+
+export_csv_json(adjudicados)
