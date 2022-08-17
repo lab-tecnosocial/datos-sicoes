8110f9728cca2b4690dce2607b41362f65489250
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-08-16 21:39:45 -0400

    moviendo fichas

--- scripts/R/organizar-fichas.R
+++ scripts/R/organizar-fichas.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+# Mover a carpetas especificas
+
 lista_temas_fichas <- readRDS("temas/lista_temas_fichas.Rds")
 
 dir_source <- "scripts/python/todo_fichas/"
@@ -13,3 +15,10 @@ walk2(lista_temas_fichas, names(lista_temas_fichas),
       walk(fichas, ~file.copy(str_c(dir_source, .x), str_c(dir_new, .x)))
     }
   )
+
+# Mover general
+
+dir_source <- "scripts/python/fichas_simp_unique/"
+dir_target <- "scripts/python/fichas_falta/"
+
+walk(totales_faltantes$cuce, ~file.copy(str_c(dir_source, .x, ".pdf"), str_c(dir_target, .x, ".pdf")))
